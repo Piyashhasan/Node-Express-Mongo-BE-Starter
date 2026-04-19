0e685848566cf6da5821cc7cd58f2a5fbbbc4c11
Piyashhasan <piyashhasan38@gmail.com>
2026-04-20 00:13:40 +0600

    feat: DB Name store in constant.js file and use it in dbConnection.js file

--- src/DB/dbConnection.js
+++ src/DB/dbConnection.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constant";
 
 const connectionDB = async () => {
     try {
         // -- use it for Mongo Atlas configuration --
-        //  const connection = await mongoose.connect(
-        //      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w46pkek.mongodb.net/ExamFlow?appName=Cluster0`
-        //  );
+        // const connection = await mongoose.connect(
+        //     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w46pkek.mongodb.net/${DB_NAME}?appName=Cluster0`
+        // );
 
         // -- use it for local development configuration --
         const connectionEstablished = await mongoose.connect(
-            process.env.MONGO_DB_URI
+            `${process.env.MONGO_DB_URI}/${DB_NAME}`
         );
 
         console.log("✅ MongoDB connected");
